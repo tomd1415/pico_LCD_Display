7862e7fe0f8630fdeea7e79cd8d7b2289e71af3d
tomd1415 <tom_duguid@yahoo.com>
2021-07-15 00:45:31 +0100

    added support for LCD displays that have the rw pin tied to ground

--- h_files/pico_LCD_display.h
+++ h_files/pico_LCD_display.h
@@ -5,33 +5,40 @@
 #include "pico/binary_info.h"
 
 struct lcd_display {
+	//if rw_pin is connected to ground set rw_pin > 40
 	uint e_pin;
 	uint rs_pin;
 	uint rw_pin;
 	uint db_pins[8];
 };
 
-void set_pins(struct lcd_display display, uint rs, uint rw, char pin_state);
-void init_display(struct lcd_display display);
-void init_pins(struct lcd_display display);
-void wait_for_busy_flag(struct lcd_display display);
-void display_msg(struct lcd_display display, char *message);
-void jump_to_line_2(struct lcd_display display);
-void jump_to_pos(struct lcd_display display, uint pos);
+void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state);
+void lcd_init_display(struct lcd_display display);
+void lcd_init_pins(struct lcd_display display);
+void lcd_wait_for_busy_flag(struct lcd_display display);
+void lcd_display_msg(struct lcd_display display, char *message);
+void lcd_jump_to_line_2(struct lcd_display display);
+void lcd_jump_to_pos(struct lcd_display display, uint pos);
+void lcd_set_cursor(struct lcd_display display, bool cursor);
 
 
-void set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
+void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
 // set the pins for the LCD Display
 // char pin_state is a byte to hold the D7 to D0 pins
 // rs - register select pin
 // rw - read/write pin
 {
-	
-	wait_for_busy_flag(display);
+	if (display.rw_pin <= 30)
+		lcd_wait_for_busy_flag(display);
+	else
+		sleep_us(50);
+
 	gpio_put(display.e_pin, 0);
 	sleep_us(1);
 	gpio_put(display.rs_pin, rs);
-	gpio_put(display.rw_pin, rw);
+	if (display.rw_pin <= 30)
+		gpio_put(display.rw_pin, rw);
+	
 	for (int bit_idx = 0; bit_idx < 8; bit_idx++){
 		gpio_put(display.db_pins[bit_idx],  pin_state & (0x80 >> bit_idx));
 		
@@ -44,34 +51,42 @@ void set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
 }
 
 
-void init_display(struct lcd_display display)
+void lcd_init_display(struct lcd_display display)
 {
 	// Page 45 of HD44780 Datasheet as reference for following 8 bit initialization
 	sleep_ms(45);
-	set_pins(display, 0, 0, 0x30);
+	lcd_set_pins(display, 0, 0, 0x30);
 	sleep_ms(5);
-	set_pins(display, 0, 0, 0x30);
+	lcd_set_pins(display, 0, 0, 0x30);
 	sleep_us(120);
-	set_pins(display, 0, 0, 0x30);
+	lcd_set_pins(display, 0, 0, 0x30);
+	
+	if (display.rw_pin > 30)
+		sleep_ms(2);
 	// Function set to 8-bit operation; 2 line display
-	set_pins(display, 0, 0, 0x38);
+	lcd_set_pins(display, 0, 0, 0x38);
 	// Turn on the display and cursor
-	set_pins(display, 0, 0, 0x0e);
+	lcd_set_pins(display, 0, 0, 0x0e);
 	// Set entry mode to increment and not shift display
-	set_pins(display, 0, 0, 0x06);
+	lcd_set_pins(display, 0, 0, 0x06);
 	// Clear screen and move to home
-	set_pins(display, 0, 0, 0x01);
+	lcd_set_pins(display, 0, 0, 0x01);
+
+	if (display.rw_pin > 30)
+		sleep_ms(2);
 
 }
 
-void init_pins(struct lcd_display display)
+void lcd_init_pins(struct lcd_display display)
 {
 	gpio_init(display.e_pin);
 	gpio_set_dir(display.e_pin, GPIO_OUT);
 	gpio_init(display.rs_pin);
 	gpio_set_dir(display.rs_pin, GPIO_OUT);
-	gpio_init(display.rw_pin);
-	gpio_set_dir(display.rw_pin, GPIO_OUT);
+	if (display.rw_pin <= 30) {
+		gpio_init(display.rw_pin);
+		gpio_set_dir(display.rw_pin, GPIO_OUT);
+	}
 	for (int i = 0; i < 8; i++)
 	{
 		gpio_init(display.db_pins[i]);
@@ -80,16 +95,16 @@ void init_pins(struct lcd_display display)
 
 }
 
-void display_msg(struct lcd_display display, char *message)
+void lcd_display_msg(struct lcd_display display, char *message)
 {
 	for (int byte_idx = 0; byte_idx < strlen(message); byte_idx++) {
 		char d_byte = message[byte_idx];
-		set_pins(display, 1, 0, d_byte);
+		lcd_set_pins(display, 1, 0, d_byte);
 	}
 
 }
 
-void wait_for_busy_flag(struct lcd_display display)
+void lcd_wait_for_busy_flag(struct lcd_display display)
 // Waits for the LCD's busy flag to clear.
 
 {
@@ -134,11 +149,20 @@ void wait_for_busy_flag(struct lcd_display display)
 	}
 }
 
-void jump_to_line_2(struct lcd_display display)
+void lcd_jump_to_line_2(struct lcd_display display)
 {
-    	jump_to_pos(display, 40);
+    	lcd_jump_to_pos(display, 40);
 }
 
-void jump_to_pos(struct lcd_display display, uint pos) {
-	set_pins(display, 0,0, (0x80 + pos));
+void lcd_jump_to_pos(struct lcd_display display, uint pos) {
+	lcd_set_pins(display, 0,0, (0x80 + pos));
+}
+
+void lcd_set_cursor(struct lcd_display display, bool cursor) {
+//cursor = true --> cursor on
+//cursor = false --> cursor off
+	if (cursor)
+		lcd_set_pins(display, 0, 0, 0x0e);
+	else
+		lcd_set_pins(display, 0, 0, 0x0c);
 }

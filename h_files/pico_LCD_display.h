cc6d20e445fafa562e95bce4baa734fdd5b6698c
tomd1415 <tom_duguid@yahoo.com>
2021-07-15 15:52:22 +0100

    Added support for using only 4 data (DB) pins

--- h_files/pico_LCD_display.h
+++ h_files/pico_LCD_display.h
@@ -12,7 +12,7 @@ struct lcd_display {
 	char db_pins[8];
 };
 
-void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state);
+void lcd_set_pins(struct lcd_display display, char rs, char rw, char pin_state);
 void lcd_init_display(struct lcd_display display);
 void lcd_init_pins(struct lcd_display display);
 void lcd_wait_for_busy_flag(struct lcd_display display);
@@ -22,7 +22,7 @@ void lcd_jump_to_pos(struct lcd_display display, uint pos);
 void lcd_set_cursor(struct lcd_display display, bool cursor);
 
 
-void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
+void lcd_set_pins(struct lcd_display display, char rs, char rw, char pin_state)
 // set the pins for the LCD Display
 // char pin_state is a byte to hold the D7 to D0 pins
 // rs - register select pin
@@ -40,9 +40,31 @@ void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
 	if (display.rw_pin <= 30)
 		gpio_put(display.rw_pin, rw);
 	
-	for (int bit_idx = 0; bit_idx < 8; bit_idx++){
-		gpio_put(display.db_pins[bit_idx],  pin_state & (0x80 >> bit_idx));
+	//setting the data pins (DB) here
+	if ((display.db_pins[4] > 30) || (display.db_pins[5] > 30) || (display.db_pins[6] > 30) || (display.db_pins[7] > 30))
+	{
+		//set the first 4 bits
+		for (int bit_idx = 0; bit_idx < 4; bit_idx++){
+			gpio_put(display.db_pins[bit_idx],  pin_state & (0x80 >> bit_idx));
 		
+		}
+
+		sleep_us(1);
+		gpio_put(display.e_pin,1);
+		sleep_us(1);
+		gpio_put(display.e_pin,0);
+		
+		//set the second 4 bits
+		for (int bit_idx = 4; bit_idx < 8; bit_idx++){
+			gpio_put(display.db_pins[bit_idx-4],  pin_state & (0x80 >> bit_idx));
+		
+		}
+	} else {
+
+		for (int bit_idx = 0; bit_idx < 8; bit_idx++){
+			gpio_put(display.db_pins[bit_idx],  pin_state & (0x80 >> bit_idx));
+			
+		}
 	}
 	sleep_us(1);
 	gpio_put(display.e_pin,1);
@@ -51,32 +73,73 @@ void lcd_set_pins(struct lcd_display display, uint rs, uint rw, char pin_state)
 	
 }
 
+void lcd_set_pins_4_only(struct lcd_display display, char rs, char rw, char pin_state)
+{
+
+	sleep_us(50);
+
+	gpio_put(display.e_pin, 0);
+	sleep_us(1);
+	gpio_put(display.rs_pin, rs);
+	if (display.rw_pin <= 30)
+		gpio_put(display.rw_pin, rw);
+	
+	for (int bit_idx = 0; bit_idx < 4; bit_idx++){
+		gpio_put(display.db_pins[bit_idx],  pin_state & (0x80 >> bit_idx));
+		
+	}
+	sleep_us(1);
+	gpio_put(display.e_pin,1);
+	sleep_us(1);
+	gpio_put(display.e_pin,0);
+}
 
 void lcd_init_display(struct lcd_display display)
 /* Sets up the display as per the Datasheet */
 {
-	// Page 45 of HD44780 Datasheet as reference for following 8 bit initialization
-	sleep_ms(45);
-	lcd_set_pins(display, 0, 0, 0x30);
-	sleep_ms(5);
-	lcd_set_pins(display, 0, 0, 0x30);
-	sleep_us(120);
-	lcd_set_pins(display, 0, 0, 0x30);
+	// Page 46 of HD44780 Datasheet as reference for following 4 bit initialization
+	if ((display.db_pins[4] > 30) || (display.db_pins[5] > 30) || (display.db_pins[6] > 30) || (display.db_pins[7] > 30))
+	{
+		sleep_ms(20);
+		lcd_set_pins_4_only(display, 0, 0, 0x30);
+		sleep_ms(5);
+		lcd_set_pins_4_only(display, 0, 0, 0x30);
+		sleep_us(120);
+		lcd_set_pins_4_only(display, 0, 0, 0x30);
+		sleep_us(120);
+		lcd_set_pins_4_only(display, 0, 0, 0x20); 
+		sleep_ms(2);
+		lcd_set_pins_4_only(display, 0, 0, 0x20);
+		lcd_set_pins_4_only(display, 0, 0, 0x80);
+		sleep_ms(2);
+
+		
+	} else {
 	
-	if (display.rw_pin > 30)
-		sleep_ms(2);
-	// Function set to 8-bit operation; 2 line display
-	lcd_set_pins(display, 0, 0, 0x38);
-	// Turn on the display and cursor
-	lcd_set_pins(display, 0, 0, 0x0e);
-	// Set entry mode to increment and not shift display
-	lcd_set_pins(display, 0, 0, 0x06);
-	// Clear screen and move to home
-	lcd_set_pins(display, 0, 0, 0x01);
+		// Page 45 of HD44780 Datasheet as reference for following 8 bit initialization
+		sleep_ms(45);
+		lcd_set_pins(display, 0, 0, 0x30);
+		sleep_ms(5);
+		lcd_set_pins(display, 0, 0, 0x30);
+		sleep_us(120);
+		lcd_set_pins(display, 0, 0, 0x30);
+		
+		if (display.rw_pin > 30)
+			sleep_ms(2);
+		// Function set to 8-bit operation; 2 line display
+		lcd_set_pins(display, 0, 0, 0x38);
 
-	if (display.rw_pin > 30)
-		sleep_ms(2);
+	}
 
+		// Turn on the display and cursor
+		lcd_set_pins(display, 0, 0, 0x0e);
+		// Set entry mode to increment and not shift display
+		lcd_set_pins(display, 0, 0, 0x06);
+		// Clear screen and move to home
+		lcd_set_pins(display, 0, 0, 0x01);
+
+		if (display.rw_pin > 30)
+			sleep_ms(2);	
 }
 
 void lcd_init_pins(struct lcd_display display)
@@ -84,6 +147,7 @@ void lcd_init_pins(struct lcd_display display)
 {
 	gpio_init(display.e_pin);
 	gpio_set_dir(display.e_pin, GPIO_OUT);
+	gpio_put(display.e_pin, 0);
 	gpio_init(display.rs_pin);
 	gpio_set_dir(display.rs_pin, GPIO_OUT);
 	// only set up the RW pin if it is connected
@@ -91,10 +155,20 @@ void lcd_init_pins(struct lcd_display display)
 		gpio_init(display.rw_pin);
 		gpio_set_dir(display.rw_pin, GPIO_OUT);
 	}
-	for (int i = 0; i < 8; i++)
-	{
-		gpio_init(display.db_pins[i]);
-		gpio_set_dir(display.db_pins[i], GPIO_OUT);
+	
+	if ((display.db_pins[4] > 30) || (display.db_pins[5] > 30) || (display.db_pins[6] > 30) || (display.db_pins[7] > 30)) {
+		for (int i = 0; i < 4; i++)
+		{
+			gpio_init(display.db_pins[i]);
+			gpio_set_dir(display.db_pins[i], GPIO_OUT);
+		}
+	} else {
+	
+		for (int i = 0; i < 8; i++)
+		{
+			gpio_init(display.db_pins[i]);
+			gpio_set_dir(display.db_pins[i], GPIO_OUT);
+		}
 	}
 
 }
@@ -119,11 +193,16 @@ void lcd_wait_for_busy_flag(struct lcd_display display)
 	char start_state_DB[8];
 	bool start_state_RS;
 	bool start_state_RW;
+	uint num_bits = 8;
+
+	if ((display.db_pins[4] > 30) || (display.db_pins[5] > 30) || (display.db_pins[6] > 30) || (display.db_pins[7] > 30)) {
+		num_bits = 4;
+	}
 
 	start_state_RS = gpio_get(display.rs_pin);
 	start_state_RW = gpio_get(display.rw_pin);
 
-	for (int i = 0; i < 8; i++){
+	for (int i = 0; i < num_bits; i++){
 		start_state_DB[i] = gpio_get(display.db_pins[i]);
 	}
 
@@ -143,6 +222,13 @@ void lcd_wait_for_busy_flag(struct lcd_display display)
 		busy = gpio_get(display.db_pins[0]);
 		sleep_us(1);
 		gpio_put(display.e_pin, 0);
+		if (num_bits == 4) {
+			sleep_us(1);
+			gpio_put(display.e_pin, 1);
+			sleep_us(1);
+			gpio_put(display.e_pin, 0);
+			sleep_us(1);
+		}
 	
 	
 	}
@@ -152,7 +238,7 @@ void lcd_wait_for_busy_flag(struct lcd_display display)
 	// Reset the pins to their orgional state
 	gpio_put(display.rw_pin, start_state_RW);
 	gpio_put(display.rs_pin, start_state_RS);
-	for (int i = 0; i < 8; i++){
+	for (int i = 0; i < num_bits; i++){
 		gpio_put(display.db_pins[i], start_state_DB[i]);
 	}
 }
